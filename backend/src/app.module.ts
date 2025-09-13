fbc13bafd4d4760b5b7b4e39af2309ce4e8ebce3
aartoni <alessio@artoni.org>
2025-09-13 10:48:51 +0200

    feat(be): finalize database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { readFileSync } from 'fs';
+
+function readVarFromFile(varName: string): string {
+  const filePath = process.env[varName];
+  if (!filePath) {
+    throw new Error(`${varName} env var is not set`);
+  }
+  return readFileSync(filePath, 'utf8').trim();
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: readVarFromFile('DB_PASSWORD_FILE'),
+      database: 'piece',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
